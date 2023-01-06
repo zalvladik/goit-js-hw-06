f93b8a2f67b9dd5bce4b34efd3720a40a09e8326
zalvladik <zalipkav@ukr.net>
2023-01-06 12:09:59 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
 const borderSetting = document.querySelector('#validation-input')
 borderSetting.addEventListener('blur', () =>{
-    if(borderSetting.value.length > borderSetting.dataset.length) {
-        borderSetting.classList.remove('valid')
-        borderSetting.classList.add('invalid')
-    } else {
+    if(borderSetting.value.length == borderSetting.dataset.length) {
         borderSetting.classList.remove('invalid')
         borderSetting.classList.add('valid')
+    } else {
+        borderSetting.classList.remove('valid')
+        borderSetting.classList.add('invalid')
     }
     console.log(borderSetting)
   }
